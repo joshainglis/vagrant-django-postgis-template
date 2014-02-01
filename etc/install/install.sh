ec36b5a3fdf2c9da99b9dc707d5a546c99e326de
joshainglis <a1BWl4gHTyIBoS>
2014-02-01 14:59:07 +1000

    Switched to en_AU locale

--- etc/install/install.sh
+++ etc/install/install.sh
@@ -6,22 +6,22 @@
 
 PROJECT_NAME=$1
 
-DB_NAME=$PROJECT_NAME
-VIRTUALENV_NAME=$PROJECT_NAME
+DB_NAME=${PROJECT_NAME}
+VIRTUALENV_NAME=${PROJECT_NAME}
 
-PROJECT_DIR=/home/vagrant/$PROJECT_NAME
-VIRTUALENV_DIR=/home/vagrant/.virtualenvs/$PROJECT_NAME
+PROJECT_DIR=/home/vagrant/${PROJECT_NAME}
+VIRTUALENV_DIR=/home/vagrant/.virtualenvs/${PROJECT_NAME}
 
 PGSQL_VERSION=9.1
 
 # Need to fix locale so that Postgres creates databases in UTF-8
-cp -p $PROJECT_DIR/etc/install/etc-bash.bashrc /etc/bash.bashrc
-locale-gen en_GB.UTF-8
+cp -p ${PROJECT_DIR}/etc/install/etc-bash.bashrc /etc/bash.bashrc
+locale-gen en_AU.UTF-8
 dpkg-reconfigure locales
 
-export LANGUAGE=en_GB.UTF-8
-export LANG=en_GB.UTF-8
-export LC_ALL=en_GB.UTF-8
+export LANGUAGE=en_AU.UTF-8
+export LANG=en_AU.UTF-8
+export LC_ALL=en_AU.UTF-8
 
 # Install essential packages from Apt
 apt-get update -y
@@ -38,8 +38,8 @@ apt-get install -y git
 
 # Postgresql
 if ! command -v psql; then
-    apt-get install -y postgresql-$PGSQL_VERSION libpq-dev
-    cp $PROJECT_DIR/etc/install/pg_hba.conf /etc/postgresql/$PGSQL_VERSION/main/
+    apt-get install -y postgresql-${PGSQL_VERSION} libpq-dev
+    cp ${PROJECT_DIR}/etc/install/pg_hba.conf /etc/postgresql/${PGSQL_VERSION}/main/
     /etc/init.d/postgresql reload
 fi
 
@@ -52,7 +52,7 @@ if [[ ! -f /usr/local/bin/virtualenv ]]; then
 fi
 
 # bash environment global setup
-cp -p $PROJECT_DIR/etc/install/bashrc /home/vagrant/.bashrc
+cp -p ${PROJECT_DIR}/etc/install/bashrc /home/vagrant/.bashrc
 su - vagrant -c "mkdir -p /home/vagrant/.pip_download_cache"
 
 # Node.js, CoffeeScript and LESS
@@ -74,17 +74,17 @@ fi
 # ---
 
 # postgresql setup for project
-createdb -Upostgres $DB_NAME
+createdb -Upostgres ${DB_NAME}
 
 # virtualenv setup for project
-su - vagrant -c "/usr/local/bin/virtualenv $VIRTUALENV_DIR && \
-    echo $PROJECT_DIR > $VIRTUALENV_DIR/.project && \
-    PIP_DOWNLOAD_CACHE=/home/vagrant/.pip_download_cache $VIRTUALENV_DIR/bin/pip install -r $PROJECT_DIR/requirements.txt"
+su - vagrant -c "/usr/local/bin/virtualenv ${VIRTUALENV_DIR} && \
+    echo ${PROJECT_DIR} > ${VIRTUALENV_DIR}/.project && \
+    PIP_DOWNLOAD_CACHE=/home/vagrant/.pip_download_cache ${VIRTUALENV_DIR}/bin/pip install -r ${PROJECT_DIR}/requirements.txt"
 
-echo "workon $VIRTUALENV_NAME" >> /home/vagrant/.bashrc
+echo "workon ${VIRTUALENV_NAME}" >> /home/vagrant/.bashrc
 
 # Set execute permissions on manage.py, as they get lost if we build from a zip file
-chmod a+x $PROJECT_DIR/manage.py
+chmod a+x ${PROJECT_DIR}/manage.py
 
 # Django project setup
-su - vagrant -c "source $VIRTUALENV_DIR/bin/activate && cd $PROJECT_DIR && ./manage.py syncdb --noinput && ./manage.py migrate"
+su - vagrant -c "source ${VIRTUALENV_DIR}/bin/activate && cd ${PROJECT_DIR} && ./manage.py syncdb --noinput && ./manage.py migrate"
